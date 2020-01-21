11151581fa1564c06b6d8e256c225e7ea058d5d1
Elkabonga <tisgo.slip@hotmail.com>
2020-01-21 14:00:09 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const http = require('http')
+
+mongoose.connect('mongodb+srv://elkabonga:zasx1452@cluster0-hdtfh.mongodb.net/week10?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const app = express()
+const server = http.Server(app)
+
+const routes = require('./routes.js')
+const { setupWebsocket } = require('./websocket.js')
+
+setupWebsocket(server)
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+server.listen(3333)
